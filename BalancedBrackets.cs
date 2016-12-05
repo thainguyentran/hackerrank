f83a3b49aca527ced77f554f706866bc875def1d
Tran Thai Nguyen <thainguyentran@icloud.com>
2016-12-05 21:48:51 +0700

    Create BalancedBrackets.cs

---
+++ BalancedBrackets.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+namespace HackerRank
+{
+    class Solution
+    {
+
+        static void Main(String[] args)
+        {
+            Functions f = new Functions();
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string expression = Console.ReadLine();
+                Console.WriteLine(f.CheckBrackets(expression));
+            }
+            Console.Read();
+        }
+    }
+    class Functions
+    {
+        const string UNMATCHED = "NO";
+        const string ALLMATCHED = "YES";
+        private Stack<char> brackets;
+
+        public string CheckBrackets(string line)
+        {
+            brackets = new Stack<char>();
+            if (line.IndexOfAny("{}[]()".ToCharArray()) != -1)
+            {
+                foreach (char i in line)
+                {
+                    if (i == '{' || i == '(' || i == '[') brackets.Push(i);
+
+                    if (i == '}' || i == ')' || i == ']')
+                    {
+                        if (brackets.Count == 0)
+                            return UNMATCHED;
+                        else
+                        {
+                            char leftBrackets = brackets.Pop();
+
+                            if ((i == '}' && leftBrackets != '{')
+                            || (i == ')' && leftBrackets != '(')
+                            || (i == ']' && leftBrackets != '['))
+                                return UNMATCHED;
+                        }
+                    }
+                }
+                if (brackets.Count == 0)
+                    return ALLMATCHED;
+                else return UNMATCHED;
+            }
+            return UNMATCHED;
+        }
+    }
+}
